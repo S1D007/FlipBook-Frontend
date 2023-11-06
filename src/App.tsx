363891fee3f7ee5d23213dd6c6b417db523415bd
S1D007 <s1d.official011001011@gmail.com>
2023-11-06 23:38:32 +0530

    changed the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import{ useState, useEffect } from 'react';
-import axios from 'axios';
-import { useDropzone } from 'react-dropzone';
-import io from 'socket.io-client';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useDropzone } from "react-dropzone";
+import io from "socket.io-client";
 
-const socket = io('https://13.233.225.40.nip.io/',{
-  transports: ['websocket'],
+const socket = io("https://13.233.225.40.nip.io/", {
+  transports: ["websocket"],
 });
 
 function App() {
   const [file, setFile] = useState<any>(null);
   const [jobId, setJobId] = useState(null);
-  const [progress, setProgress] = useState(0);
   const [flipbookUrl, setFlipbookUrl] = useState(null);
   const [sent, setSent] = useState(false);
   const onDrop = (acceptedFiles: any) => {
-    setFile(acceptedFiles[0]);
+    if (acceptedFiles && acceptedFiles.length) {
+      const video = acceptedFiles[0];
+      const videoElement = document.createElement("video");
+
+      videoElement.preload = "metadata";
+      videoElement.onloadedmetadata = function () {
+        window.URL.revokeObjectURL(videoElement.src);
+        if (videoElement.videoWidth > videoElement.videoHeight) {
+          setFile(video);
+        } else {
+          alert("Please upload a landscape video.");
+        }
+      };
+      videoElement.src = URL.createObjectURL(video);
+    }
   };
 
   useEffect(() => {
-    socket.on('jobProgress', ({ jobId, progress }) => {
-      if (jobId === jobId) {
-        setProgress(progress);
-      }
-    });
-
-    socket.on('zipReady', ({ url }) => {
+    socket.on("zipReady", ({ url }) => {
       setFlipbookUrl(url);
       setSent(false);
     });
@@ -33,21 +40,25 @@ function App() {
   const handleUpload = async () => {
     setSent(true);
     const formData = new FormData();
-    formData.append('video', file);
+    formData.append("video", file);
 
     try {
-      const response = await axios.post('https://13.233.225.40.nip.io/flipbook?count=40', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        params: {
-          socketId: socket.id,
-        },
-      });
+      const response = await axios.post(
+        "https://13.233.225.40.nip.io/flipbook?count=40",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          params: {
+            socketId: socket.id,
+          },
+        }
+      );
 
       setJobId(response.data.jobId);
     } catch (error) {
-      console.error('Error occurred during upload:', error);
+      console.error("Error occurred during upload:", error);
     }
   };
 
@@ -55,42 +66,71 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-xs">
-        {
-          !file && (
-            <div {...getRootProps()} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center justify-center border-2 border-dashed border-gray-400 cursor-pointer">
-              <input {...getInputProps()} />
-              <p className="text-gray-700 text-center">Drag 'n' drop a video file here, or click to select a file</p>
-            </div>
-          )
-        }
-        {
-          file && (
-            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center justify-center">
-              <p className="text-gray-700 text-center">{file.name}</p>
-            </div>
-          )
-        }
+      <img
+        src="https://www.gokapture.com/img/gokapture/favicon.png"
+        className="h-20 flex self-center"
+      />
+      <div className="w-full max-w-4xl flex justify-center items-center flex-col">
+        {!file && (
+          <div
+            {...getRootProps()}
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center justify-center border-2 border-dashed border-gray-400 cursor-pointer max-w-xs"
+          >
+            <input {...getInputProps()} />
+            <p className="text-gray-700 text-center">
+              Drag 'n' drop a video file here, or click to select a file
+            </p>
+          </div>
+        )}
+        {file && (
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center justify-center space-y-4">
+            <video
+              controls
+              src={URL.createObjectURL(file)}
+              className="w-full rounded-xl shadow-lg"
+            />
+            <p className="text-gray-700 text-center">{file.name}</p>
+          </div>
+        )}
+
         <div className="flex justify-center space-x-4">
+          {!flipbookUrl && file && !sent && (
+            <button
+              onClick={handleUpload}
+              className="bg-black hover:bg-black-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Upload
+            </button>
+          )}
+          {sent && (
+            <h1 className="text-center text-xl text-medium">Processing...</h1>
+          )}
+        
+          {flipbookUrl && (
+            <button
+              onClick={() => {
+                window.open(flipbookUrl, "_blank");
+              }}
+              className="text-white px-10 py-2 rounded-md bg-black hover:bg-black mt-4"
+            >
+              Download
+            </button>
+          )}
           {
-            file && !sent && (
-              <button
-                onClick={handleUpload}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-              >
-                Upload
-              </button>
-            )
-          }
-          {
-            sent && (
-              <h1 className="text-center">Processing...</h1>
-            )
+            flipbookUrl && <button
+            onClick={()=>{
+              setFile(null);
+              setFlipbookUrl(null);
+              setSent(false);
+              setJobId(null)
+            }}
+            className="text-white px-10 py-2 rounded-md bg-red-500 hover:bg-red-600 mt-4"
+          >
+            Reset
+          </button>
           }
         </div>
-        {progress > 0 && <p className="text-center my-4">Job Progress: {progress}%</p>}
-        {flipbookUrl && <p className="text-center my-4">Flipbook URL: {flipbookUrl}</p>}
       </div>
     </div>
   );
